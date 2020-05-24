17725c60c745f3c89eb0218dfae12e7dcf5e8e57
Jennifer Guo <jenniferguo@Jennifers-MBP-3.T-mobile.com>
2020-05-23 21:29:38 -0400

    oauth works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Primary App
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <a href='http://localhost:8888'> Login to Spotify </a>
+      </div>
+    );
+  }
 }
 
 export default App;
