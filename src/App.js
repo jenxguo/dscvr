b35ffa345a049c80a010542ee20a81bc043e5b09
Jennifer Guo <jenniferguo@Jennifers-MBP-3.T-mobile.com>
2020-05-23 22:07:56 -0400

    rerouting fix 2

--- src/App.js
+++ src/App.js
@@ -34,7 +34,12 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <a href='http://localhost:8888'> Login to Spotify </a>
+        {!this.state.loggedIn && (
+          <a href='http://localhost:8888'> Login to Spotify </a>
+        )}
+        {this.state.loggedIn && (
+          <p>testing</p>
+        )}
       </div>
     );
   }
